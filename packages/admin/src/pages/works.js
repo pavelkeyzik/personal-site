557bdbb8aeacb369658061cb60e460e8833ba3ab
Pavel Keyzik <pavel.keyzik@gmail.com>
2020-05-12 22:55:22 +0300

    (admin) Added works page header with breadcrumbs, add button, and search input

--- packages/admin/src/pages/works.js
+++ packages/admin/src/pages/works.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Breadcrumb,
+  Heading,
+  Flex,
+  ButtonPrimary,
+  TextInput,
+} from '@primer/components';
+import { PlusIcon, SearchIcon } from '@primer/octicons-v2-react';
 
 function WorksPage() {
-  return <p>WorksPage</p>;
+  return (
+    <React.Fragment>
+      <Breadcrumb>
+        <Breadcrumb.Item as={Link} to="/">
+          Home
+        </Breadcrumb.Item>
+        <Breadcrumb.Item as={Link} to="/works" selected>
+          Works
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Flex alignItems="center" justifyContent="space-between" mt={2}>
+        <Heading>Works</Heading>
+        <div>
+          <TextInput mr={4} icon={SearchIcon} placeholder="Find work" />
+          <ButtonPrimary>
+            <PlusIcon /> Add new work
+          </ButtonPrimary>
+        </div>
+      </Flex>
+    </React.Fragment>
+  );
 }
 
 export { WorksPage };
